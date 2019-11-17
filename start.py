f27c44412577e6a4da9219390933e9f2abb30e9f
Кропотухин Алексей <nameies@yandex.ru>
2019-11-17 15:16:44 +0300

    Added default structure of the game on pygame

---
+++ start.py
@@ -0,0 +1,27 @@
+import sys
+import pygame
+
+
+def main():
+    size = (800, 600)
+    black = (0, 0, 0)
+
+    pygame.init()
+    screen = pygame.display.set_mode(size)
+
+    game_over = False
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+
+        screen.fill(black)
+
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
