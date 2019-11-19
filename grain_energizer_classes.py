38854b25fb485cff146e4d27820a06429151c21d
Olga Arnautova <ya.vinnik.olga@gmail.com>
2019-11-19 20:44:05 +0300

    Grain and Energizer classed added

---
+++ grain_energizer_classes.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Grain:
+
+	def __init__(self, center_x, center_y, radius = 2, color = (255, 255, 0)):
+		self.center_x = center_x
+		self.center_y = center_y
+		self.radius = radius
+		self.color = color
+
+	def draw(self, screen):
+		pygame.draw.circle(screen, self.color,(self.center_x, self.center_y), self.radius)
+
+
+class Energizer(Grain):
+
+	def __init__(self, center_x, center_y, radius = 2, color = (255, 255, 0)):
+		super().__init__(center_x, center_y)
+		self.radius = 7
+
+	def draw(self, screen):
+		pygame.draw.circle(screen, self.color,(self.center_x, self.center_y), self.radius)
