1fad81d38095e4134dfa9fe917579fcb97091de2
Gorshkov Danila <degorshkov@gmail.com>
2019-11-28 22:22:06 +0300

    Add spawn list of grains

--- objects/grain_spawn.py
+++ objects/grain_spawn.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import pygame
 import sys
-from field.py import *
+from field import pole_xy, get_pos_in_field, z, size
+
+grain_array = []
 
 
 class Grain:
-    grain_array = []
 
     def __init__(self, center_x, center_y, radius=2, color=(255, 255, 0)):
         self.center_x = center_x
@@ -18,10 +19,9 @@ class Grain:
 
     def append(self):
         for count in range(0, 239):
-            get_pos_in_field(x, y)
-            self.center_x = xx
-            self.center_y = yy
-            self.grainArray.append(Grain((self.center_x, self.center_y), self.radius, self.color))
+            x = j*z + z//2
+            y = i*z + z//2
+            grain_array.append(Grain(x,y))
 
 
 # class Energizer(Grain):
@@ -35,21 +35,21 @@ class Grain:
 
 
 # Функция постановки зёрен на поле
-def spawn_grain(pole_xy, grain_array):
-    count = 0
+def spawn_grain(xx, yy, pole_xy, grain_array):
     for i in range(35):
-        j = 0
         for j in range(27):
-            if pole_xy[i][j] == 0 and pole_xy[i][j] == 3:
-                pole_xy[i][j] = grain_array[count]
+            if pole_xy[yy][xx] == 0 and pole_xy[yy][xx] == 3:
+                pole_xy[yy][xx] = grain_array
 
 
-def check_grain(xx, yy, grain_array):
+def check_grain(x, y, xx, yy, grain_array):
     for i in range(len(grain_array)):
-        if (pole_xy[i][j] == grain_array[i]):
-            return 'true'
-        else:
-            return 'false'
+        x = xx * z + z//2
+        y = yy * z + z//2
+    if grain.center_y == y and grain.center_x == x:
+        return True
+    else:
+        return False
 
 
 def main():
@@ -59,9 +59,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode(size)
 
-    for i in range(len(pole_xy[yy][xx])):
-        Grain.draw(xx, yy)
-    # g1 = Grain(100, 100)
+    for grain in grain_array:
+        grain.draw(screen)
+
     # g2 = Energizer(50, 50)
 
     game_over = False
@@ -72,7 +72,7 @@ def main():
 
         screen.fill(black)
 
-        g1.draw(screen)
+        #g1.draw(screen)
 
         pygame.display.flip()
         pygame.time.wait(10)
