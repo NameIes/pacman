b2ad7a38eea18b96397adae2c8d5860739e39ca4
Кропотухин Алексей <nameies@yandex.ru>
2019-11-30 21:23:45 +0300

    Ghosts refactored

--- objects/ghost.py
+++ objects/ghost.py
@@ -1,31 +1,45 @@
 import pygame
-import random
+from random import randint
 from objects.field import pole_xy, get_pos_in_field, is_cell_centre
 from objects.lee_pathfinder import *
+from copy import deepcopy
 
 
 class GhostBase:
-    scared = 'res/img/scared.png'
+    images = {
+        'scared': pygame.image.load('res/img/scared.png'),
+        'blinky': [pygame.image.load('res/img/blinky_0.png'),
+                   pygame.image.load('res/img/blinky_1.png')],
+        'clyde': [pygame.image.load('res/img/clyde_0.png'),
+                  pygame.image.load('res/img/clyde_1.png')],
+        'inky': [pygame.image.load('res/img/inky_0.png'),
+                 pygame.image.load('res/img/inky_1.png')],
+        'pinky': [pygame.image.load('res/img/pinky_0.png'),
+                  pygame.image.load('res/img/pinky_1.png')]
+    }
+    direction_vector = {'up': [0, -1], 'down': [0, 1], 'left': [-1, 0], 'right': [1, 0]}
 
-    def __init__(self, x, y, direction='up', anim_speed=10, pacman_rect=None, speed=1):
-        self.direction_vector = {'up': [0, -1], 'down': [0, 1], 'left': [-1, 0], 'right': [1, 0]}
+    def __init__(self, x, y, ghost_name, pacman_obj=None, move_speed=1, anim_speed=10, direction='up'):
+        self.pacman_obj = pacman_obj
 
-        self.pacman_rect = pacman_rect  # Будет нужно для ИИ движения
+        self.images = GhostBase.images[ghost_name]
+        self.current_image = self.images[0]
+        self.rect = self.current_image.get_rect()
 
-        self.images = None
-        self.image = None
-        self.rect = None
+        self.rect.x = x
+        self.rect.y = y
+        self.in_field_x = None
+        self.in_field_y = None
 
         self.anim_speed = anim_speed
-        self.timer = 0
+        self.anim_timer = 0
         self.anim_stage = True
 
         self.direction = direction
-        self.direction_speed = self.direction_vector.copy()
-        self.set_speed(speed)
+        self.direction_speed = deepcopy(GhostBase.direction_vector)
+        self.set_move_speed(move_speed)
 
-        self.is_killed = False
-        self._is_dead = False
+        self.is_dead = False
         self.is_dead_process = False
         self.is_dead_path = []
 
@@ -33,12 +47,11 @@ class GhostBase:
         self.scared_time = 500
         self.scared_timer = 0
 
-    def set_speed(self, speed):
+    def set_move_speed(self, speed):
+        self.direction_speed = deepcopy(GhostBase.direction_vector)
         for item in self.direction_speed:
-            if self.direction_speed[item][0] != 0:
-                self.direction_speed[item][0] = speed if self.direction_speed[item][0] > 0 else -speed
-            if self.direction_speed[item][1] != 0:
-                self.direction_speed[item][1] = speed if self.direction_speed[item][1] > 0 else -speed
+            self.direction_speed[item][0] *= speed
+            self.direction_speed[item][1] *= speed
 
     def scared_move(self):
         # TODO: Реализовать движение при испуге
@@ -46,22 +59,23 @@ class GhostBase:
 
     def _find_path(self):
         # Делаем копию поля, где стены -1, а дорожки 0
-        m_field = prepare_field([1, 2], pole_xy.copy())
+        prepared_field = prepare_field([1, 2], pole_xy.copy())
 
         # Получаем конечную точку
         spawn_points = [(17, 12), (17, 13), (17, 14), (17, 15)]
-        r_point = spawn_points[random.randint(0, 3)]
+        end_point = spawn_points[randint(0, 3)]
 
         # Распространяем волну алгоритма Ли
-        x, y = get_pos_in_field(self.rect.centerx, self.rect.centery)
-        m_field = push_wave(y, x, 1, len(pole_xy), len(pole_xy[0]), m_field, r_point)
+        prepared_field = push_wave(self.in_field_y, self.in_field_x, 1, len(pole_xy), len(pole_xy[0]), prepared_field,
+                                   end_point)
 
         # Восстанавливаем путь
-        self.is_dead_path = get_path((y, x), r_point, m_field, len(m_field), len(m_field[0]))
+        self.is_dead_path = get_path((self.in_field_y, self.in_field_x), end_point, prepared_field,
+                                     len(prepared_field[0]), len(prepared_field))
         self.is_dead_path = modify_path(self.is_dead_path)
 
-    def _push(self):
-        # Двигает привидение после поворота, из-за того что даже is_cell_centre срабатывает несколько раз
+    def _push_towards(self):
+        # Двигает привидение в сторону движения, иначе привидения поворачивает несколько раз в одной развилке
         if self.direction == 'up':
             self.rect.y -= 2
         if self.direction == 'down':
@@ -72,51 +86,47 @@ class GhostBase:
             self.rect.x += 2
 
     def _set_death_direction(self):
-        # Проверяем вернулся ли призрак в клетку
-        if len(self.is_dead_path) == 0:
-            self.is_dead_process = False
-            self._is_dead = False
-            self.is_killed = False
-            self.scared = False
-            self.set_speed(1)
-            return
-
         # Устанавливаем нужное направление в зависимости от координаты
-        x, y = get_pos_in_field(self.rect.centerx, self.rect.centery)
-        if y < self.is_dead_path[0][1]:
+        if self.in_field_y < self.is_dead_path[0][1]:
             self.direction = 'down'
-        if y > self.is_dead_path[0][1]:
+        if self.in_field_y > self.is_dead_path[0][1]:
             self.direction = 'up'
-        if x < self.is_dead_path[0][0]:
+        if self.in_field_x < self.is_dead_path[0][0]:
             self.direction = 'right'
-        if x > self.is_dead_path[0][0]:
+        if self.in_field_x > self.is_dead_path[0][0]:
             self.direction = 'left'
 
     def death_move(self):
         # Вызываем функцию поиска пути один раз
         if not self.is_dead_process:
-            self.set_speed(2)
+            self.set_move_speed(2)
             self._find_path()
             self._set_death_direction()
             self.is_dead_process = True
 
         # Проверяем достиг ли призрак нужной точки
-        x, y = get_pos_in_field(self.rect.centerx, self.rect.centery)
-        if (x, y) == self.is_dead_path[0]:
+        if (self.in_field_x, self.in_field_y) == self.is_dead_path[0]:
             if is_cell_centre(self.rect.centerx, self.rect.centery):
                 self.is_dead_path.pop(0)
+
+                # Проверяем вернулся ли призрак в клетку
+                if len(self.is_dead_path) == 0:
+                    self.is_dead_process = False
+                    self.is_dead = False
+                    self.scared = False
+                    self.set_move_speed(1)
+                    return
+
                 self._set_death_direction()
-                self._push()
+                self._push_towards()
 
     def kill(self):
-        self.is_killed = True
+        self.is_dead = True
 
     def escape_move(self):
-        x, y = get_pos_in_field(self.rect.centerx, self.rect.centery)
-
-        if x not in [13, 14] and is_cell_centre(self.rect.centerx, self.rect.centery):
-            self.direction = 'left' if x > 13 else 'right'
-        elif x in [13, 14] and is_cell_centre(self.rect.centerx, self.rect.centery):
+        if self.in_field_x not in [13, 14] and is_cell_centre(self.rect.centerx, self.rect.centery):
+            self.direction = 'left' if self.in_field_x > 13 else 'right'
+        elif self.in_field_x in [13, 14] and is_cell_centre(self.rect.centerx, self.rect.centery):
             self.direction = 'up'
 
     def get_rand_direction(self, x, y):
@@ -131,42 +141,45 @@ class GhostBase:
         if pole_xy[y][x + 1] not in [1, 4] and self.direction != 'left':
             p_dirs.append('right')
 
-        return p_dirs[random.randint(0, len(p_dirs) - 1)]
+        return p_dirs[randint(0, len(p_dirs) - 1)]
 
     def process_logic(self):
+        # Узнаем позицию в поле
+        self.in_field_x, self.in_field_y = get_pos_in_field(self.rect.centerx, self.rect.centery)
+
+        # Движение в направлении
         self.rect.x += self.direction_speed[self.direction][0]
         self.rect.y += self.direction_speed[self.direction][1]
 
-        if self.is_killed and is_cell_centre(self.rect.centerx, self.rect.centery):
-            self._is_dead = True
-        if self._is_dead:
+        # Возращение в клетку после смерти
+        if self.is_dead and is_cell_centre(self.rect.centerx, self.rect.centery):
             self.death_move()
             return
 
-        self.image_controller()
-
-        x, y = get_pos_in_field(self.rect.centerx, self.rect.centery)
-        if pole_xy[y][x] == 4:
+        # Адекватный выход из клетки
+        if pole_xy[self.in_field_y][self.in_field_x] == 4:
             self.escape_move()
             return
 
+        # Режим паники
         if self.scared:
             self.scared_timer += 1
             if self.scared_timer == self.scared_time:
                 self.scared = False
                 self.scared_timer = 0
 
+        # Алгоритм поведения привидений
         # OLD ALGORITHM =====================================================
-        if y == 17:
-            if x == 0:
+        if self.in_field_y == 17:
+            if self.in_field_x == 0:
                 self.direction = 'right'
-            if x == 27:
+            if self.in_field_x == 27:
                 self.direction = 'left'
-        if pole_xy[y][x] == 3:
+        if pole_xy[self.in_field_y][self.in_field_x] == 3:
             if is_cell_centre(self.rect.centerx, self.rect.centery):
-                self.direction = self.get_rand_direction(x, y)
+                self.direction = self.get_rand_direction(self.in_field_x, self.in_field_y)
 
-                self._push()
+                self._push_towards()
         # ===================================================================
 
     def _set_pupil_pos(self, screen, left, right):
@@ -174,22 +187,24 @@ class GhostBase:
         pygame.draw.rect(screen, (22, 0, 255), (self.rect[0] + right[0], self.rect[1] + right[1], 4, 5))
 
     def image_controller(self):
-        if not self.scared or self._is_dead:  # Анимация
-            self.timer += 1
-            if self.timer == self.anim_speed:
-                self.image = self.images[1 if self.anim_stage else 0]
+        if not self.scared or self.is_dead:  # Анимация
+            self.anim_timer += 1
+            if self.anim_timer == self.anim_speed:
+                self.current_image = self.images[1 if self.anim_stage else 0]
 
                 self.anim_stage = not self.anim_stage
-                self.timer = 0
+                self.anim_timer = 0
         elif self.scared:  # Напуган, когда съедено большое зерно
-            self.image = pygame.image.load(GhostBase.scared)
+            self.current_image = GhostBase.images['scared']
 
     def process_draw(self, screen):
-        # Когда съеден пакманом, на базу бегут только глаза
-        if not self._is_dead:
-            screen.blit(self.image, self.rect)
+        # Когда съеден пакманом, в клетку бегут только глаза
+        if not self.is_dead:
+            screen.blit(self.current_image, self.rect)
+            self.image_controller()
 
-        if self.scared and not self._is_dead:
+        # Режим паники
+        if self.scared and not self.is_dead:
             return
 
         # Глаза
--- objects/ghosts.py
+++ objects/ghosts.py
@@ -3,45 +3,20 @@ from objects.ghost import GhostBase
 
 
 class Blinky(GhostBase):
-    def __init__(self, x, y, direction='up', anim_speed=10, pacman_rect=None, speed=1):
-        super().__init__(x, y, direction, anim_speed, pacman_rect, speed)
-        self.images = [pygame.image.load('res/img/blinky_0.png'),
-                       pygame.image.load('res/img/blinky_1.png')]
-        self.image = self.images[0]
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+    def __init__(self, x, y, pacman_obj=None, move_speed=1, anim_speed=10, direction='up'):
+        super().__init__(x, y, 'blinky', pacman_obj, move_speed, anim_speed, direction)
 
 
-# При создании отдельного алгоритма движения, можно будет отнаследовать от Blinky
 class Clyde(GhostBase):
-    def __init__(self, x, y, direction='up', anim_speed=10, pacman_rect=None, speed=1):
-        super().__init__(x, y, direction, anim_speed, pacman_rect, speed)
-        self.images = [pygame.image.load('res/img/clyde_0.png'),
-                       pygame.image.load('res/img/clyde_1.png')]
-        self.image = self.images[0]
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+    def __init__(self, x, y, pacman_obj=None, move_speed=1, anim_speed=10, direction='up'):
+        super().__init__(x, y, 'clyde', pacman_obj, move_speed, anim_speed, direction)
 
 
 class Inky(GhostBase):
-    def __init__(self, x, y, direction='up', anim_speed=10, pacman_rect=None, speed=1):
-        super().__init__(x, y, direction, anim_speed, pacman_rect, speed)
-        self.images = [pygame.image.load('res/img/inky_0.png'),
-                       pygame.image.load('res/img/inky_1.png')]
-        self.image = self.images[0]
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+    def __init__(self, x, y, pacman_obj=None, move_speed=1, anim_speed=10, direction='up'):
+        super().__init__(x, y, 'inky', pacman_obj, move_speed, anim_speed, direction)
 
 
 class Pinky(GhostBase):
-    def __init__(self, x, y, direction='up', anim_speed=10, pacman_rect=None, speed=1):
-        super().__init__(x, y, direction, anim_speed, pacman_rect, speed)
-        self.images = [pygame.image.load('res/img/pinky_0.png'),
-                       pygame.image.load('res/img/pinky_1.png')]
-        self.image = self.images[0]
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+    def __init__(self, x, y, pacman_obj=None, move_speed=1, anim_speed=10, direction='up'):
+        super().__init__(x, y, 'pinky', pacman_obj, move_speed, anim_speed, direction)
