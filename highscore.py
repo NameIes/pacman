2745e385da6e0a2190039950b760f42487f4c8af
Vsevolod Shipunov <shipunov_stalk@mail.ru>
2019-12-04 01:53:35 +0300

    Added file reading

--- highscore.py
+++ highscore.py
@@ -1,31 +1,36 @@
+import array
 import sys
 import pygame
 
 from objects.field import black
 from ready import Text
 
+
 def highscore_table(screen):
+    with open('records.txt') as f: #чтение из файла. Файл пример
+        data = list(map(int, f.read().split())) #чтение из файла
+    data = sorted(data, reverse=True) #сортировка по возрастанию
     text_object1 = Text("Highscore", 50)
     text_object1.update_position(50, 10)
-    text_object2 = Text('1.', 30)
+    text_object2 = Text('1.   ' + str(data[0]), 30)
     text_object2.update_position(10, 70)
-    text_object3 = Text('2.', 30)
+    text_object3 = Text('2.   ' + str(data[1]), 30)
     text_object3.update_position(10, 100)
-    text_object4 = Text('3.', 30)
+    text_object4 = Text('3.   ' + str(data[2]), 30)
     text_object4.update_position(10, 130)
-    text_object5 = Text('4.', 30)
+    text_object5 = Text('4.   ' + str(data[3]), 30)
     text_object5.update_position(10, 160)
-    text_object6 = Text('5.', 30)
+    text_object6 = Text('5.   ' + str(data[4]), 30)
     text_object6.update_position(10, 190)
-    text_object7 = Text('6.', 30)
+    text_object7 = Text('6.   ' + str(data[5]), 30)
     text_object7.update_position(10, 220)
-    text_object8 = Text('7.', 30)
+    text_object8 = Text('7.   ' + str(data[6]), 30)
     text_object8.update_position(10, 250)
-    text_object9 = Text('8.', 30)
+    text_object9 = Text('8.   ' + str(data[7]), 30)
     text_object9.update_position(10, 280)
-    text_object10 = Text('9.', 30)
+    text_object10 = Text('9.   ' + str(data[8]), 30)
     text_object10.update_position(10, 310)
-    text_object11 = Text('10.', 30)
+    text_object11 = Text('10.  ' + str(data[9]), 30)
     text_object11.update_position(10, 340)
 
     game_over = False
